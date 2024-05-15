b55cd9a0e5732fbb78a8bb77d553e022046785d9
MedABada <badaproj3.2024@gmail.com>
2024-05-15 20:29:09 +0100

    v0.0.3b

--- Scripts/loadingScript.js
+++ Scripts/loadingScript.js
@@ -1,9 +1,4 @@
 window.addEventListener("load", () => {
-    setTimeout(remove,5000);
-});
-
-function remove()
-{
     var loader = document.getElementById('loader');
     loader.style.display = 'none';
 
@@ -41,4 +36,48 @@ function remove()
             div.parentNode.removeChild(div);
         }, 1000); // wait for the transition duration
     });
+});
+
+
+function load()
+{
+    var screenWidth = screen.width;
+    var loader = document.getElementById('loader');
+
+    if (screenWidth >= 721) {
+    // Hide the loader
+    loader.style.display = 'none';
+
+    // Run the RGBA script
+    RGBA(`
+        vec2 uv = gl_FragCoord.xy/resolution - 0.5;
+        uv.x *= resolution.x/resolution.y;
+        if (uv.x > 0.0) uv.y = -uv.y;
+        
+        float r = 0.2*cos(2.0*atan(uv.y, uv.x));
+        float d = dot(uv, uv);
+        float s = 0.0015/sqrt(d);
+        float c = 1.0 - smoothstep(r, r+s, d)
+                        - smoothstep(r, r-s, d);
+        
+        gl_FragColor = vec4(c, c, c, 1.0);
+        
+        vec3 p = vec3(0.5, 0.2, 0.33);
+        r = sqrt(r);
+        d = pow(d, 0.2) * 25.0;
+        
+        for (float i = 1e-6; i < 1.0; i += 0.05) {
+            float t = 1.0 + time*1.5 + i;
+            vec2 uv2 = uv + vec2(cos(t), sin(t)) * r;
+            vec3 color = vec3(sin(t*4.0), cos(t*3.0), sin(t*7.0));
+            c = smoothstep(i/d, 0.0, length(uv2));
+            gl_FragColor += vec4(p * (1.0 + color) * c, 1.);   
+        }
+    `, {
+        record: false
+    });
+    } else {
+    // Show the loader
+    loader.style.display = 'block';
+    }
 }
